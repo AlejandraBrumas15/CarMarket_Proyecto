13e143722431bcea847aff3b4e611d4088738b3d
AlejandraBrumas15 <alejandra.brumas@utp.ac.pa>
2026-07-03 12:47:36 -0500

    primer commit d eprueba

--- CarMarket_Proyecto/Form1.cs
+++ CarMarket_Proyecto/Form1.cs
@@ -16,5 +16,10 @@ namespace CarMarket_Proyecto
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if () { }
+        }
     }
 }
