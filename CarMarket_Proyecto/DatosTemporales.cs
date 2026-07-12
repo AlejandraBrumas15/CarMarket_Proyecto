4131f244440c6bef2d27beafa014378d01d9fd17
Gabito-06 <gab0hg06@gmail.com>
2026-07-12 13:35:29 -0500

    Create DatosTemporales.cs

---
+++ CarMarket_Proyecto/DatosTemporales.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace CarMarket_Proyecto
+{
+    public static class DatosTemporales
+    {
+        public static Usuario UsuarioActual;
+
+        public static List<Publicacion> ListaPublicaciones =
+            new List<Publicacion>();
+    }
+}
