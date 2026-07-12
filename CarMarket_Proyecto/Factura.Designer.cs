afc37fb182c9b4f7c64522446e3df51334729f7d
Gabito-06 <gab0hg06@gmail.com>
2026-07-12 15:35:35 -0500

    Update Factura.Designer.cs

--- CarMarket_Proyecto/Factura.Designer.cs
+++ CarMarket_Proyecto/Factura.Designer.cs
@@ -181,6 +181,7 @@
             this.button1.TabIndex = 65;
             this.button1.Text = "Realizar compra";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Factura
             // 
